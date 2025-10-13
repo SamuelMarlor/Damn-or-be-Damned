de0f4425a81020c2b91a1cc5f6a47b5129be7264
SamuelMarlor <23652403@stu.mmu.ac.uk>
2025-10-13 14:23:14 +0100

    changimg wall writing

--- Damn or be Damned/Assets/Scripts/Door.cs
+++ Damn or be Damned/Assets/Scripts/Door.cs
@@ -1,67 +1,67 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
-{
-    public bool isOpen = false;
-    [SerializeField]
-    private bool isRotating = true;
-    [SerializeField]
-    private float speed = 1f;
-    [Header("Roatation Configs")]
-    [SerializeField]
-    private float rotationAmount = 90f;
-    [SerializeField]
-    private float forwardDirection = 0f;
+//public class Door : MonoBehaviour
+//{
+//    public bool isOpen = false;
+//    [SerializeField]
+//    private bool isRotating = true;
+//    [SerializeField]
+//    private float speed = 1f;
+//    [Header("Roatation Configs")]
+//    [SerializeField]
+//    private float rotationAmount = 90f;
+//    [SerializeField]
+//    private float forwardDirection = 0f;
 
-    private Vector3 startRotation;
-    private Vector3 Forward;
+//    private Vector3 startRotation;
+//    private Vector3 Forward;
 
-    private Coroutine animationCoroutine;
+//    private Coroutine animationCoroutine;
 
-    private void Awake()
-    {
-        startRotation = transform.rotation.eulerAngles;
-        Forward = transform.right;
-    }
+//    private void Awake()
+//    {
+//        startRotation = transform.rotation.eulerAngles;
+//        Forward = transform.right;
+//    }
 
-    public void Open (Vector3 UserPosition)
-    {
-        if (!isOpen) return;
-        {
-            if (animationCoroutine != null)
-                StopCoroutine(animationCoroutine);
-        }
+//    public void Open (Vector3 UserPosition)
+//    {
+//        if (!isOpen) return;
+//        {
+//            if (animationCoroutine != null)
+//                StopCoroutine(animationCoroutine);
+//        }
         
-        if (isRotatingDoor)
-        {
-            float dot = Vector3.Dot(Forward, (UserPosition - transform.position).normalized);
-            Debug.log($"Dot: {dot.ToString("N3")}");
-            animationCoroutine = StartCoroutine(DoRotationOpen(dot));
-        }
-    }
+//        if (isRotatingDoor)
+//        {
+//            float dot = Vector3.Dot(Forward, (UserPosition - transform.position).normalized);
+//            Debug.log($"Dot: {dot.ToString("N3")}");
+//            animationCoroutine = StartCoroutine(DoRotationOpen(dot));
+//        }
+//    }
 
-    private IEnumerator DoRotationOpen(float ForwardAmount)
-    {
-        Quaternion startRotation = transform.rotation;
-        Quaternion endRotation;
+//    private IEnumerator DoRotationOpen(float ForwardAmount)
+//    {
+//        Quaternion startRotation = transform.rotation;
+//        Quaternion endRotation;
 
-        if (ForwardAmount >= forwardDirection)
-        {
-            endRotation = Quaternion.Euler(new Vector3(0, startRotationRotation.y - rotationAmount,0));
-        }
-        else
-        {
-            endRotation = Quaternion.Euler(new Vector3(0, startRotationRotation.y + rotationAmount, 0));
-        }
+//        if (ForwardAmount >= forwardDirection)
+//        {
+//            endRotation = Quaternion.Euler(new Vector3(0, startRotationRotation.y - rotationAmount,0));
+//        }
+//        else
+//        {
+//            endRotation = Quaternion.Euler(new Vector3(0, startRotationRotation.y + rotationAmount, 0));
+//        }
 
-        isOpen = true;
+//        isOpen = true;
 
-        float time = 0;
-        while (time < 1)
-        {
-            transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
-            yield return null;
-            time += Time.deltaTime * speed;
-        }
-    }
-}
+//        float time = 0;
+//        while (time < 1)
+//        {
+//            transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+//            yield return null;
+//            time += Time.deltaTime * speed;
+//        }
+//    }
+//}
