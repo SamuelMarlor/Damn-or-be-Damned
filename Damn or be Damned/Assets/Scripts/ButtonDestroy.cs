357f36145da6701c5a952f4143cbf68d3a9da224
GeorgeHammond553 <23662672@stu.mmu.ac.uk>
2025-10-16 23:43:57 +0100

    button tweak (REady to build)

--- Damn or be Damned/Assets/Scripts/ButtonDestroy.cs
+++ Damn or be Damned/Assets/Scripts/ButtonDestroy.cs
@@ -6,16 +6,22 @@ public class ButtonDestroy : MonoBehaviour
 {
     public void DestroyButton()
     {
-        GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("Room1");
+        // Get this GameObject's tag dynamically
+        string objectTag = gameObject.tag;
+
+        // Find all objects with the same tag
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(objectTag);
+
         int pressedLayer = LayerMask.NameToLayer("Pressed");
 
-        foreach (GameObject obj in roomObjects)
+        foreach (GameObject obj in taggedObjects)
         {
             // Set parent object's layer to "Pressed" if it has a parent
             if (obj.transform.parent != null)
             {
                 obj.transform.parent.gameObject.layer = pressedLayer;
             }
+
             // Destroy the object
             Destroy(obj);
         }
